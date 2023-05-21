8b709f6e7e245a1dbe880f787c3f4101a0dcb83f
kyosu-1 <sho013039@gmail.com>
2023-05-21 22:51:38 +0900

    main.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"github.com/labstack/echo/v4"
+
+	"github.com/kyosu-1/ims/gen/api"
+	"github.com/kyosu-1/ims/internal/handler"
+)
+
+func main() {
+	e := echo.New()
+	h := handler.NewHandlers()
+
+	api.RegisterHandlers(e, h)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
