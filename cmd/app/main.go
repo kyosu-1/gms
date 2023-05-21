d9b38348d07304e423259efd0cb8e412351d4a68
kyosu-1 <sho013039@gmail.com>
2023-05-21 23:11:51 +0900

    use config

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"github.com/labstack/echo/v4"
 
 	"github.com/kyosu-1/ims/gen/api"
 	"github.com/kyosu-1/ims/internal/handler"
+	"github.com/kyosu-1/ims/internal/config"
 )
 
 func main() {
+	cfg, err := config.New()
+	if err != nil {
+		panic(err)
+	}
+
 	e := echo.New()
 	h := handler.NewHandlers()
 
 	api.RegisterHandlers(e, h)
 
-	e.Logger.Fatal(e.Start(":8080"))
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", cfg.Server.Port)))
 }
