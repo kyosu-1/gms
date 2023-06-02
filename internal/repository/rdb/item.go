fff1338774100d24e5bcf15c41ce70710129691d
kyosu-1 <sho013039@gmail.com>
2023-06-02 12:51:36 +0900

    インタフェースのみ定義

--- internal/repository/rdb/item.go
+++ internal/repository/rdb/item.go
@@ -1,7 +1,11 @@
 package rdb
 
 import (
+	"context"
+
 	"github.com/jmoiron/sqlx"
+
+	"github.com/kyosu-1/ims/internal/model"
 )
 
 type ItemRepository struct {
@@ -10,4 +14,20 @@ type ItemRepository struct {
 
 func NewItemRepository(db *sqlx.DB) *ItemRepository {
 	return &ItemRepository{db: db}
+}
+
+func (r *ItemRepository) FindAllItems(ctx context.Context) ([]*model.Item, error) {
+	return nil, nil
+}
+
+func (r *ItemRepository) FindItemByID(ctx context.Context, id string) (*model.Item, error) {
+	return nil, nil
+}
+
+func (r *ItemRepository) SaveItem(ctx context.Context, item *model.Item) error {
+	return nil
+}
+
+func (r *ItemRepository) DeleteItem(ctx context.Context, id string) error {
+	return nil
 }
