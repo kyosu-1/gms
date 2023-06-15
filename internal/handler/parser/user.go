56fc471a29dd3f7fbaf996fee1cabd628d0b2862
kyosu-1 <sho013039@gmail.com>
2023-06-16 06:58:00 +0900

    fix user

--- internal/handler/parser/user.go
+++ internal/handler/parser/user.go
@@ -1,11 +1,1 @@
 package parser
-
-type User struct {
-	ID             int
-	Name           string
-	HashedPassword string
-}
-
-func (u *User) IsSamePassword(password string) bool {
-	return u.HashedPassword == password
-}
--- internal/model/user.go
+++ internal/model/user.go
@@ -1,7 +1,11 @@
 package model
 
 type User struct {
-	ID       string
-	Name     string
-	Password string
+	ID             string
+	Name           string
+	HashedPassword string
+}
+
+func (u *User) IsSamePassword(password string) bool {
+	return u.HashedPassword == password
 }
