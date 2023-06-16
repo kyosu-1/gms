0eb57073f202cc88a3ce2085154a596891d895bf
kyosu-1 <sho013039@gmail.com>
2023-06-16 09:13:43 +0900

    User

--- internal/handler/parser/user.go
+++ internal/handler/parser/user.go
@@ -1,1 +1,13 @@
 package parser
+
+import (
+	"github.com/kyosu-1/ims/gen/api"
+	"github.com/kyosu-1/ims/internal/model"
+)
+
+func (m *Model) User(user *model.User) *api.User {
+	return &api.User{
+		Id:   user.ID,
+		Name: user.Name,
+	}
+}
