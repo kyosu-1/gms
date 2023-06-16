41b7d2eca48b0e446b4da2d3d5d6bc536bcd7c5d
kyosu-1 <sho013039@gmail.com>
2023-06-16 09:33:41 +0900

    echo contextをctxからecに命名変更

--- internal/handler/category.go
+++ internal/handler/category.go
@@ -6,20 +6,20 @@ import (
 	"github.com/kyosu-1/ims/gen/api"
 )
 
-func (h *Handlers) GetCategories(ctx echo.Context) error {
+func (h *Handlers) GetCategories(ec echo.Context) error {
 	return nil
 }
 
-func (h *Handlers) PostCategories(ctx echo.Context) error {
+func (h *Handlers) PostCategories(ec echo.Context) error {
 	return nil
 }
 
-func (h *Handlers) DeleteCategoriesCategoryID(ctx echo.Context, categoryID api.CategoryID) error {
+func (h *Handlers) DeleteCategoriesCategoryID(ec echo.Context, categoryID api.CategoryID) error {
 	// TODO: implement
 	return nil
 }
 
-func (h *Handlers) GetCategoriesCategoryID(ctx echo.Context, categoryID api.CategoryID) error {
+func (h *Handlers) GetCategoriesCategoryID(ec echo.Context, categoryID api.CategoryID) error {
 	// TODO: implement
 	return nil
 }
--- internal/handler/item.go
+++ internal/handler/item.go
@@ -6,32 +6,32 @@ import (
 	"github.com/kyosu-1/ims/gen/api"
 )
 
-func (h *Handlers) GetItems(ctx echo.Context) error {
+func (h *Handlers) GetItems(ec echo.Context) error {
 	// TODO: implement
 	return nil
 }
 
-func (h *Handlers) PostItems(ctx echo.Context) error {
+func (h *Handlers) PostItems(ec echo.Context) error {
 	// TODO: implement
 	return nil
 }
 
-func (h *Handlers) DeleteItemsItemID(ctx echo.Context, itemID api.ItemID) error {
+func (h *Handlers) DeleteItemsItemID(ec echo.Context, itemID api.ItemID) error {
 	// TODO: implement
 	return nil
 }
 
-func (h *Handlers) GetItemsItemID(ctx echo.Context, itemID api.ItemID) error {
+func (h *Handlers) GetItemsItemID(ec echo.Context, itemID api.ItemID) error {
 	// TODO: implement
 	return nil
 }
 
-func (h *Handlers) PostItemsItemIDBorrow(ctx echo.Context, itemID api.ItemID) error {
+func (h *Handlers) PostItemsItemIDBorrow(ec echo.Context, itemID api.ItemID) error {
 	// TODO: implement
 	return nil
 }
 
-func (h *Handlers) PostItemsItemIDReturn(ctx echo.Context, itemID api.ItemID) error {
+func (h *Handlers) PostItemsItemIDReturn(ec echo.Context, itemID api.ItemID) error {
 	// TODO: implement
 	return nil
 }
