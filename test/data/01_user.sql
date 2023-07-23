f6e8161da0905f92fc7f2b8cce38354ee4a01642
kyosu-1 <sho013039@gmail.com>
2023-07-23 22:11:13 +0900

    seed data

---
+++ test/data/01_user.sql
@@ -0,0 +1,7 @@
+-- Seed for user table
+INSERT INTO users(id, name, password) VALUES
+    (
+        'test-user',
+        'taro',
+        '4c716d4cf211c7b7d2f3233c941771ad0507ea5bacf93b492766aa41ae9f720d'
+    ); -- password is 'hogehoge'
---
+++ test/data/cleanup.sql
@@ -0,0 +1,13 @@
+SET
+  foreign_key_checks = 0;
+
+TRUNCATE users;
+
+TRUNCATE items;
+
+TRUNCATE categories;
+
+TRUNCATE categorizations;
+
+SET
+  foreign_key_checks = 1;
